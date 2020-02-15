8a7072f5296f8539caaa82f2f7ec84ad3f30f52c
Sumit Gautam <sghost33@gmail.com>
2020-02-15 16:55:24 +0545

    Add create new modal for usertoolbar component

--- src/views/UserList/components/UsersToolbar/UsersToolbar.js
+++ src/views/UserList/components/UsersToolbar/UsersToolbar.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
-import { Button } from '@material-ui/core';
+import { Button, Modal, TextField } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
 
 import { SearchInput } from 'components';
 
@@ -25,11 +26,65 @@ const useStyles = makeStyles(theme => ({
   },
   searchInput: {
     marginRight: theme.spacing(1)
+  },
+  modalStyle: {
+    height: '100px',
+    display: 'flex',
+    alignItems: 'flex-start',
+    marginTop: theme.spacing(1)
+  },
+  modalTitle: {
+    marginBottom: theme.spacing(1),
+    display: 'flex',
+    justifyContent: 'center',
+    textDecoration: 'none'
+  },
+  modalContents: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  modalButtons: {
+    marginTop: theme.spacing(1),
+    display: 'flex',
+    alignItems: 'center'
+  },
+  paper: {
+    position: 'absolute',
+    width: 500,
+    height: 300,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    top: '30%',
+    left: '30%',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid blue',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  formStyle: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'space-between',
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    }
   }
 }));
 
 const UsersToolbar = props => {
   const { className, ...rest } = props;
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
 
   const classes = useStyles();
 
@@ -44,10 +99,67 @@ const UsersToolbar = props => {
         <Button className={classes.exportButton}>Export</Button>
         <Button
           color="primary"
+          onClick={handleOpen}
           variant="contained"
         >
           Add user
         </Button>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+          onClose={handleClose}
+        >
+          <div className={classes.paper}>
+            <h3 id="simple-modal-title" className={classes.modalTitle}>Add New User</h3>
+            <Divider variant="middle" />
+            <form className={classes.formStyle} autoComplete="off">
+              <div className={classes.modalContents}>
+                <TextField
+                  id="outlined-name"
+                  label="Name"
+                  value="Name"
+                  variant="outlined"
+                />
+                <TextField
+                  id="outlined-name"
+                  label="Email"
+                  value="Email"
+                  variant="outlined"
+                />
+              </div>
+              <div className={classes.modalContents}>
+                <TextField
+                  id="outlined-name"
+                  label="password"
+                  value="password"
+                  variant="outlined"
+                />
+                <TextField
+                  id="outlined-name"
+                  label="Confirm Password"
+                  value="confirm-password"
+                  variant="outlined"
+                />
+              </div>
+            </form>
+            <div className={classes.modalButtons}>
+              <Button
+                onClick={handleClose}
+                variant="contained"
+              >
+                Cancel
+              </Button>
+              <Button
+                color="primary"
+                onClick={handleClose}
+                variant="contained"
+              >
+                Create
+              </Button>
+            </div>
+          </div>
+        </Modal>
       </div>
       <div className={classes.row}>
         <SearchInput
