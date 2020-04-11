9069648bdce3518bfe46052ea0b01af0e0e1210f
Sumit Gautam sghost33@gmail.com <sghost33@gmail.com>
2020-04-11 23:47:25 +1000

    Education Feature Work in Progress

--- src/views/Education/components/EducationsToolbar/EducationsToolbar.js
+++ src/views/Education/components/EducationsToolbar/EducationsToolbar.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
-import { Button } from '@material-ui/core';
+import { Button, Modal, TextField } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
 
 import { SearchInput } from 'components';
 
@@ -25,11 +26,79 @@ const useStyles = makeStyles(theme => ({
   },
   searchInput: {
     marginRight: theme.spacing(1)
+  },
+  modalStyle: {
+    height: '100px',
+    display: 'flex',
+    alignItems: 'flex-start',
+    marginTop: theme.spacing(1)
+  },
+  modalTitle: {
+    marginBottom: theme.spacing(1),
+    display: 'flex',
+    justifyContent: 'center',
+    textDecoration: 'none'
+  },
+  modalContents: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  modalButtons: {
+    marginTop: theme.spacing(1),
+    display: 'flex',
+    alignItems: 'center'
+  },
+  paper: {
+    position: 'absolute',
+    width: 500,
+    height: 300,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    top: '30%',
+    left: '30%',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid blue',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  formStyle: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'space-between',
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    }
   }
+
 }));
 
 const NewEducation = props => {
   const { className, ...rest } = props;
+  const [open, setOpen] = React.useState(false);
+  const [instituteName, setInstituteName] = React.useState({});
+  const [degreeName, setDegreeName] = React.useState({});
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const handleInstituteName = (e) => {
+    debugger
+    e.preventDefault;
+    setInstituteName(e.target.value);
+  }
+
+  const handleDegreeName = (e) => {
+    e.preventDefault;
+    setDegreeName(e.target.value);
+  }
 
   const classes = useStyles();
 
@@ -44,10 +113,67 @@ const NewEducation = props => {
         <Button className={classes.exportButton}>Export</Button>
         <Button
           color="primary"
+          onClick={handleOpen}
           variant="contained"
         >
           Add Education
         </Button>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+          onClose={handleClose}
+        >
+          <div className={classes.paper}>
+            <h3 id="simple-modal-title" className={classes.modalTitle}>Add New User</h3>
+            <Divider variant="middle" />
+            <form className={classes.formStyle} autoComplete="off">
+              <div className={classes.modalContents}>
+                <TextField
+                  id="outlined-name"
+                  label="Institute Name"
+                  value={handleInstituteName()}
+                  variant="outlined"
+                />
+                <TextField
+                  id="outlined-name"
+                  label="Degree Name"
+                  value={handleDegreeName()}
+                  variant="outlined"
+                />
+              </div>
+              <div className={classes.modalContents}>
+                <TextField
+                  id="outlined-name"
+                  label="password"
+                  value=""
+                  variant="outlined"
+                />
+                <TextField
+                  id="outlined-name"
+                  label="Confirm Password"
+                  value=""
+                  variant="outlined"
+                />
+              </div>
+            </form>
+            <div className={classes.modalButtons}>
+              <Button
+                onClick={handleClose}
+                variant="contained"
+              >
+                Cancel
+              </Button>
+              <Button
+                color="primary"
+                onClick={handleClose}
+                variant="contained"
+              >
+                Create
+              </Button>
+            </div>
+          </div>
+        </Modal>
       </div>
       <div className={classes.row}>
         <SearchInput
